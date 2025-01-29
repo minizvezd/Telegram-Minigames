94169b811cad6adb17dd0dd7aa38e241df41eb8d
formersss123 <113029608+formersss123@users.noreply.github.com>
2025-01-29 16:56:09 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const Record = mongoose.model('Record', recordSchema);
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/my_games');
+        await mongoose.connect('mongodb+srv://formersss123:WO18tYqcSD29Dzwb@cluster0.3etwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log("MongoDB connected")
     } catch(error) {
         console.error("Database connection error: ", error);
